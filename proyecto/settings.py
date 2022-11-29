77c778170e04e785b14d69d2df18999e6728a739
LuisFGG91 <luis.guerrero.godoy@gmail.com>
2022-11-29 01:12:37 -0300

    comiit

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-3!e@fq*2*9lhv=bf-vpbcf8gsv5y(ct+w($#^168m%zf5!i$k+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','52.35.60.158']
+ALLOWED_HOSTS = ['localhost','52.35.60.158:80']
 
 
 # Application definition
